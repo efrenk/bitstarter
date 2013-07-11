5011017c6db9c160f79c30c91023c0679ef47e7d
Efren Katague <ekatague@kcscon.com>
2013-07-11 07:39:27 +0000

    added index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var url = require('url');
+var fs = require('fs');
+var index = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+var route = url.parse(request.url).pathname;
+if(route == "/") {
+       route = "index.html";
+       }
+var index = fs.readFileSync(route);
+  response.writeHead(200,{"Content-Type":"text/html"});
+  response.write(index);
+  response.end();
 });
 
 var port = process.env.PORT || 5000;
